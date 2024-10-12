00ff3cb3fa42cbe8937d81a6fa788aacf32c84a7
Dang <dmdang1203@gmail.com>
2024-10-12 11:54:37 +0700

    base tiktok

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyle from '~/components/GlobalStyles';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle>
     <App />
-  </React.StrictMode>
+    </GlobalStyle>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
